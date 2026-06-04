b35423f32eebd82b59711849fffa6a31faef8403
KewodaJoanitah <kewodagoanitah@gmail.com>
2026-06-04 23:40:23 +0300

    Initial commit

--- backend/AITS_project/settings.py
+++ backend/AITS_project/settings.py
@@ -89,25 +89,26 @@ WSGI_APPLICATION = 'AITS_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+#DATABASES = {
+ 
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.postgresql',
+   #     'NAME': 'railway',
+    #    'USER': 'postgres',
+     #   'PASSWORD': 'wPSswYHgVVdqERgcpLDaWhKSODPYrvxc',
+      #  'HOST': 'shinkansen.proxy.rlwy.net',
+       # 'PORT': '56092'
+    #}
+#}
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'wPSswYHgVVdqERgcpLDaWhKSODPYrvxc',
-        'HOST': 'shinkansen.proxy.rlwy.net',
-        'PORT': '56092'
-    }
-}
-
-
-"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 
-}"""
+}
 
 
 
